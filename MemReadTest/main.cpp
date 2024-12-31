9976b30bc938320092aa4d753feeabbcac6e6963
Alexander Orlov <gilgamesh4702@gmail.com>
2024-12-31 21:48:23 +0100

    Rework mem test.

--- MemReadTest/main.cpp
+++ MemReadTest/main.cpp
@@ -1,35 +1,32 @@
 #include "Memory.hpp"
 #include <iostream>
-#include <vector>
+#include <fstream>
 
 int main() {
-    using namespace std;
-
-    wstring TARGET_PROCESS_NAME = L"Lineage2M l OrbFighter"s;
-
-    Memory memory;
-    memory.EnableDebugPrivileges();
-
-    int processId = memory.GetProcessId(TARGET_PROCESS_NAME);
-    cout << "Process ID: " << processId << endl;
-
-    HANDLE processHandle = OpenProcess(PROCESS_ALL_ACCESS, false, processId);
-    if (processHandle == 0) {
-        cerr << "Failed to open process" << endl;
-        return 1;
-    }
-
-    uintptr_t testAddress = 0x123456; // Пример тестового адреса
     try {
-        int value = memory.ReadMemory<int>(processHandle, testAddress);
-        cout << "Read int value: " << value << endl;
+        Memory memory(L"Lineage2M l OrbFighter");
 
-        float floatValue = memory.ReadMemory<float>(processHandle, testAddress + 4);
-        cout << "Read float value: " << floatValue << endl;
-    } catch (const std::exception &e) {
-        cerr << e.what() << endl;
+        memory.ReadModuleToVector("Lineage2M.exe");
+
+        const auto& moduleMemory = memory.GetModuleMemory();
+
+
+        std::ofstream outfile;
+        outfile.open("test_data.bin", std::ios::binary | std::ios::out);
+
+        outfile.write(
+            reinterpret_cast<const char*> (moduleMemory.data()), moduleMemory.size());
+
+        outfile.close();
+
+        for (size_t i = 0; i < 1024 && i < moduleMemory.size(); ++i) {
+            std::cout << std::hex << static_cast<int>(moduleMemory[i]) << " ";
+        }
+        std::cout << std::endl;
+
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
     }
 
-    CloseHandle(processHandle);
     return 0;
 }
