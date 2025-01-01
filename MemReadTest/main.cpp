da99fe44fd27495fc4214c248d54b0b6c42f076c
Alexander Orlov <gilgamesh4702@gmail.com>
2025-01-01 14:36:45 +0100

    Rewrok.

--- MemReadTest/main.cpp
+++ MemReadTest/main.cpp
@@ -7,9 +7,7 @@ int main() {
         Memory memory(L"Lineage2M l OrbFighter");
 
         memory.ReadModuleToVector("Lineage2M.exe");
-
         const auto& moduleMemory = memory.GetModuleMemory();
-
 
         std::ofstream outfile;
         outfile.open("test_data.bin", std::ios::binary | std::ios::out);
@@ -18,11 +16,6 @@ int main() {
             reinterpret_cast<const char*> (moduleMemory.data()), moduleMemory.size());
 
         outfile.close();
-
-        for (size_t i = 0; i < 1024 && i < moduleMemory.size(); ++i) {
-            std::cout << std::hex << static_cast<int>(moduleMemory[i]) << " ";
-        }
-        std::cout << std::endl;
 
     } catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
