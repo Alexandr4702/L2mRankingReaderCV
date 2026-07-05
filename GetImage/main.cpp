725bd45dac15ec3b7048ba6bb9c8302fceba5445
Alexander Orlov <gilgamesh4702@gmail.com>
2026-07-05 09:36:44 +0200

    Clean up image capture utility

--- GetImage/main.cpp
+++ GetImage/main.cpp
@@ -3,23 +3,18 @@
 #include "Tools.h"
 
 void sendMouseClick(HWND hwnd, int x, int y) {
-  // Конвертировать координаты в координаты клиента окна
   LPARAM lParam = MAKELPARAM(x, y);
 
-  // Отправить сигнал нажатия кнопки мыши (левая кнопка)
   PostMessage(hwnd, WM_LBUTTONDOWN, MK_LBUTTON, lParam);
   PostMessage(hwnd, WM_LBUTTONUP, 0, lParam);
 }
 
 void sendMouseMove(HWND hwnd, int x, int y) {
-  // Конвертировать координаты в координаты клиента окна
   LPARAM lParam = MAKELPARAM(x, y);
 
-  // Отправить сигнал движения мыши
   PostMessage(hwnd, WM_MOUSEMOVE, 0, lParam);
 }
 
-// Функция для получения клиентской области окна и преобразования координат
 void GetClientCoordinates(HWND hwnd, int &x, int &y) {
   POINT pt = {x, y};
   ScreenToClient(hwnd, &pt);
@@ -27,11 +22,9 @@ void GetClientCoordinates(HWND hwnd, int &x, int &y) {
   y = pt.y;
 }
 
-// Функция для перемещения мыши и клика с использованием SendInput
 void SendMouseInput(int x, int y, bool click = false) {
   INPUT input = {0};
 
-  // Устанавливаем координаты курсора
   input.type = INPUT_MOUSE;
   input.mi.dx = (x * 65535) / GetSystemMetrics(SM_CXSCREEN);
   input.mi.dy = (y * 65535) / GetSystemMetrics(SM_CYSCREEN);
@@ -39,11 +32,9 @@ void SendMouseInput(int x, int y, bool click = false) {
   SendInput(1, &input, sizeof(INPUT));
 
   if (click) {
-    // Эмуляция нажатия левой кнопки мыши
     input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
     SendInput(1, &input, sizeof(INPUT));
 
-    // Эмуляция отпускания левой кнопки мыши
     input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
     SendInput(1, &input, sizeof(INPUT));
   }
