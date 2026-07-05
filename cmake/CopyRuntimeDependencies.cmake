9f983b61101fc8e1d994d5b07839e7cc3962d9d4
Alexander Orlov <gilgamesh4702@gmail.com>
2026-07-05 10:46:18 +0200

    Refine build warnings and dependency bundling

--- cmake/CopyRuntimeDependencies.cmake
+++ cmake/CopyRuntimeDependencies.cmake
@@ -16,6 +16,7 @@ file(GET_RUNTIME_DEPENDENCIES
     EXECUTABLES "${EXECUTABLE}"
     RESOLVED_DEPENDENCIES_VAR resolved_dependencies
     UNRESOLVED_DEPENDENCIES_VAR unresolved_dependencies
+    CONFLICTING_DEPENDENCIES_PREFIX dependency_conflicts
     DIRECTORIES "${COMPILER_DIRECTORY}"
     PRE_EXCLUDE_REGEXES
         "api-ms-win-.*"
@@ -23,6 +24,16 @@ file(GET_RUNTIME_DEPENDENCIES
     POST_EXCLUDE_REGEXES
         "^[A-Za-z]:/[Ww][Ii][Nn][Dd][Oo][Ww][Ss]/[Ss][Yy][Ss][Tt][Ee][Mm]32/.*"
 )
+
+# A parallel build may find the same DLL both in the compiler directory and
+# already copied next to another executable. The existing file is usable.
+if(dependency_conflicts_FILENAMES)
+    foreach(filename IN LISTS dependency_conflicts_FILENAMES)
+        if(NOT EXISTS "${DESTINATION}/${filename}")
+            message(FATAL_ERROR "Conflicting runtime dependency was not previously bundled: ${filename}")
+        endif()
+    endforeach()
+endif()
 
 foreach(dependency IN LISTS resolved_dependencies)
     file(COPY "${dependency}" DESTINATION "${DESTINATION}")
